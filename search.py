537526d0f150f75639597621bd1272ac371295d1
gentoo <82971071+sprmcell@users.noreply.github.com>
2022-12-21 14:55:33 +0200

    fixed search

--- search.py
+++ search.py
@@ -7,5 +7,7 @@ from internetarchive import search_items
 # logan-paul-dead-body-re-upload
 # with the search query of your choice
 
-for i in search_items('identifier:https://archive.org/details/logan-paul-dead-body-re-upload'):
+for i in search_items('identifier:logan-paul-dead-body-re-upload'):
     print(i["identifier"])
+
+# if it prints out the identifier it means it exists, if it prints nothing it doesnt exist 
